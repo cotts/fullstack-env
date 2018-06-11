262f6af3f9646e7a82804f5967f11e16a4b60ed9
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-11 15:01:22 -0300

    List Diapers VIew Component

---
+++ web/src/components/List.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="list">
+   <ul class="list-group">
+     <li v-for="diaper of list" class="list-group-item list-group-item-action flex-column align-items-start">
+       <router-link :to="{ name: 'Edit Diaper', params: { id: diaper.id}}">
+       <h3>{{diaper.doc.name}}</h3>
+       <h4>{{ diaper.doc.description }}</h4>
+           <h5 class="list-title">
+             Sizes
+             </h5>
+         <div class="list-sizes">
+           <div v-for="size in diaper.doc.sizes">
+             <h5>{{ size.size }} <span class="badge badge-info"> {{ size.amount }}</span></h5>
+           </div>
+         </div>
+       </router-link>
+     </li>
+   </ul>
+   <p>
+     </p>
+  </div>
+</template>
+<script>
+  import axios from 'axios';
+export default {
+  data(){
+    return {
+      list: ''
+    }
+  },
+  methods: {
+    fetchData: function(){
+      const vm = this;
+      axios.get('http://127.0.0.1:3000/api/v1/diapers')
+      .then(data => vm.list = data.data);
+      }
+  },
+  mounted(){
+    this.fetchData();
+  }
+}
+</script>
+<style scoped>
+.list-group-item {
+  margin-bottom: 10px;
+  border-radius: 10px;
+}
+.list-title {
+  font-weight: bold;
+  line-height: 2em;
+  padding-top: 10px;
+}
+.list-sizes {
+  margin: 30px auto;
+  display: flex;
+  width: 50%;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
+
+
