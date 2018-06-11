4007ceb2ba603b13c387ff21b6540d0cfec82e42
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-11 15:29:33 -0300

    disabled CORS

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const bootstrap = async () => {
     .use(bodyParser.json())
     .use(helmet())
     .use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
       res.header(
         'Access-Control-Allow-Headers',
