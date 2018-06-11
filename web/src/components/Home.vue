8be48850cb037ca818b2ac721b9cbff18a5eb273
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-11 15:01:32 -0300

    Menu Component for View

---
+++ web/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu">
+      <router-link to="/diaper">
+      <button class="btn btn-lisght">Create Diaper</button>
+    </router-link>
+    <router-link to="/list">
+      <button class="btn btn-primary">List Diapers</button>
+    </router-link>
+    <router-link to="/buy">
+      <button class="btn btn-success">Buy Diapers</button>
+    </router-link>
+  </div>
+</template>
+<style scoped>
+.menu {
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
+
