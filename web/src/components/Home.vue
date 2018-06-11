9faf3b5391e0e9060b16612077e1dc160c5319c4
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-11 19:37:57 -0300

    added delete button to index

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="menu">
       <router-link to="/diaper">
-      <button class="btn btn-lisght">Create Diaper</button>
+      <button class="btn btn-lisght btn-lg">Create Diaper</button>
     </router-link>
     <router-link to="/list">
-      <button class="btn btn-primary">List Diapers</button>
+      <button class="btn btn-primary btn-lg">List Diapers</button>
     </router-link>
     <router-link to="/buy">
-      <button class="btn btn-success">Buy Diapers</button>
+      <button class="btn btn-success btn-lg">Buy Diapers</button>
+    </router-link>
+       <router-link to="/delete">
+      <button class="btn btn-danger btn-lg">Delete Diapers</button>
     </router-link>
   </div>
 </template>
