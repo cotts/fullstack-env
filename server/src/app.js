90fbfd67e16a45f5647a6a3894c610b607e64554
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-11 15:02:37 -0300

    Fix CORS

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const router = require('./web');
 
 const app = express();
-const router = require('./web');
 
 const bootstrap = async () => {
   app
     .use(bodyParser.urlencoded({ extended: true }))
     .use(bodyParser.json())
     .use(helmet())
+    .use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.header('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+      );
+      next();
+    })
     .use(router);
 
   return app;
